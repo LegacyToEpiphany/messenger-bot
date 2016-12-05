413fe5258e4057f0c558d1a389c553f504935bc2
Baptiste Dupuch <baptiste.dupuch@gmail.com>
2016-12-05 17:51:07 +0100

    Manage multiple type events

--- src/clj/movie_finder/routes/callback.clj
+++ src/clj/movie_finder/routes/callback.clj
@@ -10,7 +10,7 @@
             [movie-finder.actions.button-template :refer [button-template-route]]
             [movie-finder.actions.generic-template :refer [generic-template-route]]
             [movie-finder.actions.list-template :refer [list-template-route]]
-            [clojure.core.async :as async :refer [go chan <! >! <!! >!! close! alts! timeout]]))
+            [clojure.core.async :as async :refer [go chan <! >! <!! >!! close! alts! timeout pub sub]]))
 
 ;; ========================== WebToken validation =============================
 (defn validate-webhook-token
@@ -24,42 +24,125 @@
 
 ; ========================== Webhook Router/Handler ==========================
 
-(def app-state (atom {}))
-(def input-chan (chan 1))
-
 (def fsm (context (routes intro-route
                           button-template-route
                           generic-template-route
                           list-template-route)))
 
-;; rajouter un timeout pour éteindre close la FSM just in case
-(defn simple-fsm [chan]
-  (go
-    (loop [state :start]
-      (let [timeout (timeout 100000)
-            [entry c] (alts! [chan timeout])]
-        (if (= c chan)
-          (let [next-state (fsm entry state)]
-            (println next-state)
-            (if (= next-state :end)
-              (do
-                (println "This is the end of the FSM with the state : " entry)
-                (close! chan)
-                (swap! app-state dissoc (keyword (str (get-in entry [:sender :id])))))
-              (recur next-state)))
-          (println "Timeout..."))))))
+(def webhooks (chan 1024))
+(def app-state (atom {}))
+
+(declare fsm-process)
+
+(def to-pub (chan 1))
+(def p (pub to-pub (fn [entry]
+                     (condp #(contains? %2 %1) entry
+                       :delivery :delivery
+                       :read :read
+                       :message :message
+                       :postback :postback))))
 
 (go
   (loop []
-    (when-let [input (<! input-chan)]
-      (println "input" input)
-      (if-let [user ((keyword (str (get-in input [:sender :id]))) @app-state)]
-        (>! (:chan user) input)
-        (let [c (chan 1)]
-          (swap! app-state assoc (keyword (str (get-in input [:sender :id]))) {:chan c})
-          (simple-fsm c)
-          (>! c input)))
+    (when-let [entry (<! webhooks)]
+      (let [sender-id (keyword (str (get-in entry [:sender :id])))]
+        (if-not (sender-id @app-state)
+          (let [delivery (chan 1)
+                read (chan 1)
+                message (chan 1)
+                postback (chan 1)]
+            (swap! app-state assoc sender-id
+                   {:delivery delivery
+                    :read     read
+                    :message  message
+                    :postback postback})
+            (fsm-process delivery read message postback))))
+      (>!! to-pub entry)
       (recur))))
+
+(defn fsm-process
+  "State Machine that keeps user's state in memory."
+  [delivery read message postback]
+  (go
+    (loop [state :start]
+      (let [[entry c] (alts! [delivery read message postback])]
+        (println entry)
+        (println state)
+        (if (= c message)
+          (let [next-state (fsm entry state)]
+            (println next-state)
+            (if (= next-state :end)
+              (comment (do
+                         ;; think how we should handle user's termination
+                         (close! chan)
+                         (swap! app-state dissoc (keyword (str (get-in entry [:sender :id]))))))
+              (recur next-state))))))))
+
+(defn read-process
+  "Manage Read Inputs"
+  []
+  (let [c (chan 1)]
+    (sub p :delivery c)
+    (go
+      (loop []
+        (when-let [entry (<! c)]
+          (let [sender-id (keyword (str (get-in entry [:sender :id])))
+                read-chan (get-in @app-state [sender-id :message])]
+            (>!! read-chan entry)
+            ;; Do anything you need to with read inputs
+            (println "Read"))
+          (recur))))))
+
+(defn delivery-process
+  "Manage Delivery Inputs"
+  []
+  (let [c (chan 1)]
+    (sub p :read c)
+    (go
+      (loop []
+        (when-let [entry (<! c)]
+          (let [sender-id (keyword (str (get-in entry [:sender :id])))
+                delivery-chan (get-in @app-state [sender-id :message])]
+            (>!! delivery-chan entry)
+            ;; Do anything you need to with delivery inputs
+            (println "Delivery"))
+          (recur))))))
+
+(defn message-process
+  "Manage Message Inputs"
+  []
+  (let [c (chan 1)]
+    (sub p :message c)
+    (go
+      (loop []
+        (when-let [entry (<! c)]
+          ;; Do everything related to this event
+          (let [sender-id (keyword (str (get-in entry [:sender :id])))
+                message-chan (get-in @app-state [sender-id :message])]
+            (>!! message-chan entry))
+          ;; Do anything you need to with message inputs
+          (println "Message")
+          (recur))))))
+
+(defn postback-process
+  "Manage Postback Inputs"
+  []
+  (let [c (chan 1)]
+    (sub p :postback c)
+    (go
+      (loop []
+        (when-let [entry (<! c)]
+          (let [sender-id (keyword (str (get-in entry [:sender :id])))
+                postback-chan (get-in @app-state [sender-id :message])]
+            (>!! postback-chan entry)
+            (println "Postback"))
+          ;; Do anything you want with postback inputs
+          (recur))))))
+
+(read-process)
+(message-process)
+(postback-process)
+(delivery-process)
 
 ; ========================== Webhook Router/Handler ==========================
 (defn webhook-router
@@ -70,7 +153,7 @@
   (dorun
     (map (fn [{messaging :messaging}]
            (dorun
-             (map (fn [message] (>!! input-chan message)) messaging))) entries))
+             (map (fn [message] (>!! webhooks message)) messaging))) entries))
   (response/ok))
 
 ;; ========================== Router definition ===============================
